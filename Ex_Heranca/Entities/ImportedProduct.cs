9f4f321ecfefc2fba14b42c851dbdeb1f4f3d1df
Naiara Rodrigues <naiapr@gmail.com>
2020-09-14 14:17:13 -0300

    Inserindo classes

---
+++ Ex_Heranca/Entities/ImportedProduct.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex_Heranca.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomsFree { get; set; }
+
+        public ImportedProduct()
+        {
+
+        }
+
+        public ImportedProduct(string name, double price, double customsFree): base (name, price)
+        {
+            CustomsFree = customsFree;
+        }
+        public double TotalPrice(double price, double customsFree)
+        {
+            return price + customsFree;
+        }
+        public override string PriceTag(string name, double price)
+        {
+            return "Customs Free :" + CustomsFree;
+        }
+
+        
+
+    }
+}
---
+++ Ex_Heranca/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex_Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
